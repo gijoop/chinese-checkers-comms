e54ba670cf203658f435e4e94940c6c24403ac8d
Łukasz Kuś <luidku@gmail.com>
2024-12-09 20:26:32 +0100

    Add Message classes

---
+++ src/main/java/com/chinese_checkers/comms/Message/AcknowledgeMessage.java
@@ -0,0 +1,22 @@
+package com.chinese_checkers.comms.Message;
+
+public class AcknowledgeMessage extends Message {
+    private String info;
+
+    public AcknowledgeMessage() {
+        this("");
+    }
+
+    public AcknowledgeMessage(String info) {
+        this.type = "acknowledge";
+        this.info = info;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public String toString() {
+        return "AcknowledgeMessage: " + info;
+    }
+}
---
+++ src/main/java/com/chinese_checkers/comms/Message/MoveMessage.java
@@ -0,0 +1,28 @@
+package com.chinese_checkers.comms.Message;
+
+public class MoveMessage extends Message {
+    protected String from;
+    protected String to;
+
+    public MoveMessage() {
+        this("", "");
+    }
+
+    public MoveMessage(String from, String to) {
+        this.type = "move";
+        this.from = from;
+        this.to = to;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public String toString() {
+        return "MoveMessage: from: " + from + " to: " + to;
+    }
+}
