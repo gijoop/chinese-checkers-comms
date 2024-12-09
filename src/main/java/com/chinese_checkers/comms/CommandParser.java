0f75c177afae5bfd8b98c7ee3ebf8afc71f378f3
Adam Kostrzewski <adam.kostrzewski@hotmail.com>
2024-12-09 21:07:59 +0100

    Added CommandParser

---
+++ src/main/java/com/chinese_checkers/comms/CommandParser.java
@@ -0,0 +1,34 @@
+package com.chinese_checkers.comms;
+
+import com.chinese_checkers.comms.Message.Message;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Consumer;
+
+public class CommandParser {
+
+	private final ConcurrentHashMap<String, Consumer<Message>> commands = new ConcurrentHashMap<>();
+
+	public CommandParser() {}
+
+	public void parseCommand(Message msg) {
+		if (commands.containsKey(msg.getType())) {
+			commands.get(msg.getType()).accept(msg);
+		} else {
+			System.out.println("Command not found for type: " + msg.getType());
+		}
+	}
+
+	public void addCommand(String msgType, Consumer<Message> action) {
+		if (commands.containsKey(msgType)) {
+			commands.remove(msgType);
+		}
+		commands.put(msgType, action);
+	}
+
+	public void clearCommands() {
+		commands.clear();
+	}
+}
+
+
