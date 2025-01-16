e071de0d8953058e851ae450355c946007d11407
Adam Kostrzewski <adam.kostrzewski@hotmail.com>
2025-01-16 13:48:52 +0100

    Made possible for CommandParser to execute more than 1 callback

--- src/main/java/com/chinese_checkers/comms/CommandParser.java
+++ src/main/java/com/chinese_checkers/comms/CommandParser.java
@@ -2,28 +2,32 @@ package com.chinese_checkers.comms;
 
 import com.chinese_checkers.comms.Message.Message;
 
+import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Consumer;
 
 public class CommandParser {
 
-	private final ConcurrentHashMap<String, Consumer<Message>> commands = new ConcurrentHashMap<>();
+	private final ConcurrentHashMap<String, List<Consumer<Message>>> commands = new ConcurrentHashMap<>();
 
 	public CommandParser() {}
 
 	public void parseCommand(Message msg) {
 		if (commands.containsKey(msg.getType())) {
-			commands.get(msg.getType()).accept(msg);
+			for (Consumer<Message> action : commands.get(msg.getType())) {
+				action.accept(msg);
+			}
 		} else {
 			System.out.println("Command not found for type: " + msg.getType());
 		}
 	}
 
 	public void addCommand(String msgType, Consumer<Message> action) {
-		if (commands.containsKey(msgType)) {
-			commands.remove(msgType);
+		if (!commands.containsKey(msgType)) {
+			commands.put(msgType, new java.util.ArrayList<>());
 		}
-		commands.put(msgType, action);
+
+		commands.get(msgType).add(action);
 	}
 
 	public void clearCommands() {
